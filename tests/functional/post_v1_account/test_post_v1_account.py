0770fc344a436cca1c24a8029f2097288e3b4da5
Siarhei Mazanik <sergey.mazanik1@gmail.com>
2024-11-06 11:01:07 +0300

    fix test

--- tests/functional/post_v1_account/test_post_v1_account.py
+++ tests/functional/post_v1_account/test_post_v1_account.py
@@ -10,7 +10,7 @@ def test_post_v1_account():
     account_api = AccountApi(host='http://5.63.153.31:5051')
     login_api = LoginApi(host='http://5.63.153.31:5051')
     mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
-    login = 'smazanik12'
+    login = 'smazanik13'
     password = '123456'
     email = f'{login}@gmail.com'
     json_data = {
