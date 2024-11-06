fd43beab66388427173e6df16a8d7961e43be716
Siarhei Mazanik <sergey.mazanik1@gmail.com>
2024-11-06 19:34:02 +0300

    add more tests

--- tests/functional/post_v1_account_login/test_post_v1_account_login.py
+++ tests/functional/post_v1_account_login/test_post_v1_account_login.py
@@ -1,14 +1,16 @@
 from json import loads
 
 from dm_api_account.apis.account_api import AccountApi
+from dm_api_account.apis.login_api import LoginApi
 from api_mailhog.apis.mailhog_api import MailhogApi
 
 
-def test_post_v1_account():
+def test_post_v1_account_login():
     # Регистрация пользователя
     account_api = AccountApi(host='http://5.63.153.31:5051')
+    login_api = LoginApi(host='http://5.63.153.31:5051')
     mailhog_api = MailhogApi(host='http://5.63.153.31:5025')
-    login = 'smazanik26'
+    login = 'smazanik28'
     password = '123456'
     email = f'{login}@gmail.com'
     json_data = {
@@ -30,6 +32,25 @@ def test_post_v1_account():
     print(token)
     assert token is not None, f'Token for user {login} does not received!'
 
+    # Активация пользователя
+    response = account_api.put_v1_account_token(token=token)
+    print_log(response=response)
+    assert response.status_code == 200, 'User does not activated!'
+
+    # Авторизоваться
+    json_data = {
+        'login': login,
+        'password': password,
+        'rememberMe': True,
+    }
+    response = login_api.post_v1_account_login(json_data=json_data)
+    print_log(response=response)
+    assert response.status_code == 200, 'User does not authorize!'
+
+    # Проверить наличие токена авторизации в хедерах
+    auth_token = response.headers['X-Dm-Auth-Token']
+    assert auth_token is not None
+
 
 def get_activation_token_by_login(login, response):
     token = None
@@ -47,7 +68,7 @@ def print_log(response):
         URL: {response.request.url}
         METHOD: {response.request.method}
         JSON:   {response.request.body}
-        
+
     RESPONSE:    
         STATUS_CODE: {response.status_code}
         CONTENT: {response.content}
